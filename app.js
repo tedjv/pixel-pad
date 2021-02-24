3fdfd9eb357cb117858422b467aa42c1a9624358
Ted Verdonkschot <verdontj@gmail.com>
2021-02-24 12:11:45 -0500

    Add event listeners for mouse activity and implement drawing logic

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ function makeCells(rows, cols) {
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "cell";
-  };
+
+    //check if the user wants to draw (mouse held down and inside a cell)
+    var mouseIsDown = false
+    cell.addEventListener('mousedown', function () { mouseIsDown = true })
+    cell.addEventListener('mouseup', function () { mouseIsDown = false })
+
+    cell.addEventListener('mousemove', function(){
+      if(mouseIsDown){
+          //drawing logic, color a cell
+          cell.classList.remove('cell');
+          cell.style.backgroundColor = "black";
+      }
+  })
+  }
 };
+
+
 
 makeCells(16, 16);
