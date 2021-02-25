36204453862494f23613238983a2d0ad3cd6767b
Ted Verdonkschot <verdontj@gmail.com>
2021-02-25 00:00:21 -0500

    Add functionality to eraser button. Once clicked, the active color is set to whitesmoke

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ function makeCells(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
+    cell.innerHTML = i + 1;
     container.appendChild(cell).className = "cell";
-
 
     //set listeners to check if the user wants to draw
     var mouseIsDown = false
@@ -20,14 +20,41 @@ function makeCells(rows, cols) {
         cell.style.backgroundColor = "black";
 
         //color a cell using the value from the color picker
-        let color = document.getElementById('paletteColor').value;
+        var color = document.getElementById('paletteColor').value;
         cell.style.backgroundColor = color;
 
+        //erase cells (color white)
+        document.getElementById("erase").onclick = function () { eraser() };
+        function eraser() {
+          document.getElementById('paletteColor').value = "#F5F5F5";
+        }
       }
     })
   }
 };
 
+
+
+/* ideas, stop doing this, start planning on paper
+
+ //clear grid 
+ document.getElementById("clear").onclick = function () { clearGrid() };
+ function clearGrid() {
+   document.querySelectorAll('.cell').setAttribute('style', 'background: whitesmoke');
+ }
+
+
+//let eraser = document.getElementById('eraser').value;
+//cell.style.backgroundColor = eraser;
+
+clear grid
+document.getElementById("clear").onclick = function() {clearGrid()};
+function clearGrid() {
+  cell.style.backgroundColor = "white";
+}*/
+
 //generate initial grid size
+
+
 makeCells(16, 16);
 
