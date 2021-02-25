598e15b592f4390d2786743db34de7d52e56187c
Ted Verdonkschot <verdontj@gmail.com>
2021-02-24 21:42:14 -0500

    Add functionality to palette button. Allow the user to choose a color using the color picker, grab that value, and update the color used to fill cells with said value.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-//make cells by creating and appending divs to the grid container
 const container = document.getElementById("container");
 
+
+//make cells by creating and appending divs to the grid container
 function makeCells(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
@@ -8,21 +9,25 @@ function makeCells(rows, cols) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "cell";
 
-    //check if the user wants to draw (mouse held down and inside a cell)
+
+    //set listeners to check if the user wants to draw
     var mouseIsDown = false
     cell.addEventListener('mousedown', function () { mouseIsDown = true })
     cell.addEventListener('mouseup', function () { mouseIsDown = false })
+    cell.addEventListener('mousemove', function () {
+      if (mouseIsDown) {
+        //color a cell
+        cell.style.backgroundColor = "black";
 
-    cell.addEventListener('mousemove', function(){
-      if(mouseIsDown){
-          //drawing logic, color a cell
-          cell.classList.remove('cell');
-          cell.style.backgroundColor = "black";
+        //color a cell using the value from the color picker
+        let color = document.getElementById('paletteColor').value;
+        cell.style.backgroundColor = color;
+
       }
-  })
+    })
   }
 };
 
-
-
+//generate initial grid size
 makeCells(16, 16);
+
